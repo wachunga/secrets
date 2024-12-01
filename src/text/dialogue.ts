7f0662af0d32a4f40d24fa8711bb26e18b9381e7
David Hirtle <wachunga@gmail.com>
2024-11-30 17:18:20 -0800

    add images

--- src/text/dialogue.ts
+++ src/text/dialogue.ts
@@ -1,7 +1,38 @@
-
+const chestPoem = `To survive,
+you may have to hide,
+To breathe,
+you may have to squeeze,
+Adapt to come alive.`;
 
 export const dialogue = {
-    secret1: "Could this be part of the secret of magic? You knew these ruins were a good place to look!",
-    secretFinal: "Oh no! The ruins are collapsing! I will have to escape through the water using my newest form!"
-
-}
+  secret1: [
+    "There's an inscription on the floor.",
+    `A cycle of life,
+a natural balance comes
+to those who seek it.`,
+    "This poem leaves you with knowledge of true harmony.",
+    "Could this be part of the secret of magic? You knew these ruins were a good place to look!",
+  ],
+  chest: [
+    "Inside the chest is a weathered scroll.",
+    /* show scroll picture with poem above */
+    "The message on the scroll is showing how creatures must adapt.",
+  ],
+  spikePainting: [
+    "Something is painted here.",
+    /* show painting image */
+    "This image seems to show you the power of animal instincts.",
+  ],
+  rubbleMelody: [
+    "Suddenly a song fills the air.",
+    // play a tune
+    "The melody enlightens you with the secret of inner peace.",
+  ],
+  secretFinal: [
+    "There's a mirror on the wall.",
+    // show mirror image
+    "The mirror is reflecting your true self, revealing the magic of transformation. It was worth all the time you spent searching, for you have discovered the secret of magic!",
+    // rumble screen
+    "Oh no! The ruins are collapsing! You will have to escape through the water using your newest form!",
+  ],
+};
